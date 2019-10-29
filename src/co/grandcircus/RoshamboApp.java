3c4a11fdbf94d6ca50f9b39fd655dc8895329570
brandonbath <brandon_bath@yahoo.com>
2019-10-29 16:59:20 -0400

    Second commit - we rockgit add . (paper, scissors)

--- src/co/grandcircus/RoshamboApp.java
+++ src/co/grandcircus/RoshamboApp.java
@@ -26,24 +26,56 @@ public class RoshamboApp {
 		Scanner scan = new Scanner(System.in);
 		String userName = Validator.getString(scan, "Please enter your name: ");
 		HumanPlayer humie = new HumanPlayer(userName, scan);
-		System.out.println(humie);
 		
-		Player opponent = null;
-		String opponentName = Validator.getStringMatchingRegex(scan, "Who do you want to play (Rock or Random?)", "[rR]ock|[rR]andom");
-		switch (opponentName) {
-			case "rock":
-			case "Rock":
-				opponent = new RockPlayer("Rock");
-				break;
-			case "random":
-			case "Random":
-			default:
-				opponent = new RandomPlayer("Random");
-		}
+		System.out.println("================================================================================");
 		
+		String continueYN = "n";
+		do {
+			Player opponent = null;
+			String opponentName = Validator.getStringMatchingRegex(scan, "Who do you want to play (Rock or Random?)", "[rR]ock|[rR]andom");
+			System.out.println("");
+			
+			switch (opponentName) {
+				case "rock":
+				case "Rock":
+					opponent = new RockPlayer("Rock player");
+					break;
+				case "random":
+				case "Random":
+				default:
+					opponent = new RandomPlayer("Random player");
+			}
+			
+			Roshambo humanResult = humie.generateRoshambo();
+			Roshambo opponentResult = opponent.generateRoshambo();
+			
+			String humanWon = "lost! You're a friggen' LOSER";
+			if (humanResult == Roshambo.PAPER && opponentResult == Roshambo.ROCK) {
+				humanWon = "won";
+			} else if (humanResult == Roshambo.ROCK && opponentResult == Roshambo.SCISSORS) {
+				humanWon = "won";
+			} else if (humanResult == Roshambo.SCISSORS && opponentResult == Roshambo.PAPER) {
+				humanWon = "won";
+			} else if (humanResult == Roshambo.PAPER && opponentResult == Roshambo.PAPER) {
+				humanWon = "tied (awww)";
+			} else if (humanResult == Roshambo.ROCK && opponentResult == Roshambo.ROCK) {
+				humanWon = "tied (awww)";
+			} else if (humanResult == Roshambo.SCISSORS && opponentResult == Roshambo.SCISSORS) {
+				humanWon = "tied (awww)";
+			}
+				//All others, the human lost!
+			
+			System.out.println("");
+			System.out.println(" You chose: " + humanResult);
+			System.out.println(opponent.getName() + " chose: " + opponentResult);
+			System.out.println(humie.getName() + ", you " + humanWon + "!");
+			
+			System.out.println("================================================================================");
+			continueYN = Validator.getStringMatchingRegex(scan, "Continue (y/n)?", "[yY]|[nN]");
+		} while (continueYN.equalsIgnoreCase("y"));
 		
-		
-		//System.out.println(opponent);
+		System.out.println("================================================================================");
+		System.out.println("Goodbye!");
 	}
 
 }
